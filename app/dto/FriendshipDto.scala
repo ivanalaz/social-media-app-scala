b26457238f141ed41add68ef5245cbe9b9b5600a
Ivana Lazic <ivanalazic93@gmail.com>
2021-06-22 12:42:33 +0200

    Revert "Initial commit"
    This reverts commit ff929c81

--- app/dto/FriendshipDto.scala
+++
@@ -1,9 +0,0 @@
-package dto
-
-import play.api.libs.json.{Json, Reads}
-
-case class FriendshipDto(userId: Long)
-
-object FriendshipDto {
-  implicit val friendReads: Reads[FriendshipDto] = Json.reads[FriendshipDto]
-}
--- app/models/Comment.scala
+++
@@ -1,5 +0,0 @@
-package models
-
-import java.time.LocalDateTime
-
-case class Comment(id: Int, user: User, text: String, date: LocalDateTime, post: Post)
